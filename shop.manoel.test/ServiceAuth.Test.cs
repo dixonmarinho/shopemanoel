4b8b54051eaaddba0e5c9297413f4dda6dcbdfe0
dixonmarinho <85615406+dixonmarinho@users.noreply.github.com>
2024-10-17 23:09:39 -0300

    Correction README

---
+++ shop.manoel.test/ServiceAuth.Test.cs
@@ -0,0 +1,28 @@
+﻿using shop.manoel.shared.Interfaces;
+using shop.manoel.shared.Models.Request;
+using shop.manoel.test.Base;
+using Xunit.Abstractions;
+
+namespace shop.manoel.test
+{
+    public class ServiceAuth_Test : Base_Service_Test<Program>
+    {
+        private readonly IServiceAuth service;
+
+        public ServiceAuth_Test(ITestOutputHelper outputWriter) : base(outputWriter)
+        {
+            service = GetService<IServiceAuth>();
+        }
+
+        [Theory]
+        [InlineData("user", "123456", true)]
+        [InlineData("user", "passfake", false)]
+        [InlineData("userfake", "123456", false)]
+        [InlineData("userfake", "passfake", false)]
+        private void CalcOrderBoxAsync(string user, string pass, bool expected)
+        {
+            var response = service.Login(new UserRequest() { User = user, Pass = pass });
+            Assert.True((response != null) == expected);
+        }
+    }
+}
